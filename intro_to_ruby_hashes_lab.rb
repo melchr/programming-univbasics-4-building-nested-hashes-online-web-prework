2f4dad7393cffebc5ca48076bde9aa21546cadbd
Melissa Christman <melchrstmn@gmail.com>
2019-09-06 19:52:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,23 +8,29 @@ def monopoly_with_second_tier
   }
 end
 
-def monopoly_with_third_tier
-:railroads => {
-  :pieces => 4,
-  :rent_in_dollars => {
+def monopoly_with_third_tier ={
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
       :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>{},
+      :pennsylvania_railroad=>{},
+      :b_and_o_railroad=>{},
+      :shortline=> {}
+    }
   }
 }
 end
 
-def monopoly_with_fourth_tier
-
-end
-
-monopoly =  {
+def monopoly_with_fourth_tier =  {
   :railroads=>
   {
     :pieces=>4,
@@ -56,3 +62,4 @@ monopoly =  {
     }
   }
 }
+end
